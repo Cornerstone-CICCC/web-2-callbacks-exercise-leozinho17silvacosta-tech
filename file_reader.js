f041621bcc2ecb402d24cb6b64587d5ef6f392f9
Leo <leozinho17silvacosta@gmail.com>
2025-11-18 20:39:36 -0800

    Complete CallBack Exercise - WEB 2

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,23 @@
 const fs = require("fs");
 
+
+let firstName, lastName, age, hobbies;
+
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  firstName = data1.trim();
+    
+    fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+      lastName = data2.trim();
+
+      fs.readFile("./age.txt", "utf-8", (err, data3) => {
+        age = data3.trim();
+
+        fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+          hobbies = data4.trim().split(',');
+
+          console.log(`${firstName} ${lastName} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`)
+        })
+      })
+    })
+  })
+  
